64390618a845c939445cb6b92d2b2c725e3fc4d4
Aleksandr Petrosyan <ap886@cantab.ac.uk>
2024-02-03 18:15:12 +0400

    [feature]: Basic experiment using `iced`
    Signed-off-by: Aleksandr Petrosyan <ap886@cantab.ac.uk>

--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,64 @@
-use gtk::prelude::*;
-use relm4::prelude::*;
+use iced::{Sandbox, Element, widget::TextInput};
 
-struct App {
-    counter: u8,
+pub struct HelloWorld{
+    text: String,
+    previous_inputs: Vec<String>
 }
 
-#[derive(Debug)]
-enum Msg {
-    Increment,
-    Decrement,
+
+#[derive(Debug, Clone)]
+pub enum Message {
+    TextInputChanged(String),
+    Submit
 }
 
-#[relm4::component]
-impl SimpleComponent for App {
-    type Init = u8;
-    type Input = Msg;
-    type Output = ();
+impl Sandbox for HelloWorld {
+    type Message = Message;
 
-    view! {
-        gtk::Window {
-            set_title: Some("Simple app"),
-            set_default_size: (300, 100),
+    fn new() -> HelloWorld {
+        HelloWorld { text: "".to_owned(), previous_inputs: Vec::new() }
+    }
 
-            gtk::Box {
-                set_orientation: gtk::Orientation::Vertical,
-                set_spacing: 5,
-                set_margin_all: 5,
+    fn title(&self) -> String {
+        String::from("Hello, World!")
+    }
 
-                gtk::Button {
-                    set_label: "Increment",
-                    connect_clicked => Msg::Increment,
-                },
-
-                gtk::Button {
-                    set_label: "Decrement",
-                    connect_clicked => Msg::Decrement,
-                },
-
-                gtk::Label {
-                    #[watch]
-                    set_label: &format!("Counter: {}", model.counter),
-                    set_margin_all: 5,
-                }
-            }
+    fn update(&mut self, message: Self::Message) {
+        match message {
+            Message::TextInputChanged(string) => self.text = string,
+            Message::Submit => self.previous_inputs.push(self.text.clone())
         }
     }
 
-    // Initialize the component.
-    fn init(
-        counter: Self::Init,
-        root: &Self::Root,
-        sender: ComponentSender<Self>,
-    ) -> ComponentParts<Self> {
-        let model = App { counter };
+    fn view(&self) -> Element<Self::Message> {
+        let column = iced::widget::Column::new();
+        let text_input = TextInput::new("Hello, World!", self.text.as_ref())
+            .on_input(Message::TextInputChanged)
+            .on_submit(Message::Submit);
 
-        // Insert the code generation of the view! macro here
-        let widgets = view_output!();
+        let button = iced::widget::button("submit")
+            .on_press(Message::Submit);
 
-        ComponentParts { model, widgets }
-    }
+        let row = iced::widget::Row::new();
 
-    fn update(&mut self, msg: Self::Input, _sender: ComponentSender<Self>) {
-        match msg {
-            Msg::Increment => {
-                self.counter = self.counter.wrapping_add(1);
-            }
-            Msg::Decrement => {
-                self.counter = self.counter.wrapping_sub(1);
-            }
+        let row = row.push(text_input).push(button).padding(10).spacing(10);
+
+
+        let column = column.push(row);
+
+        let mut elements: Vec<_> = Vec::new();
+        for element in &self.previous_inputs {
+            elements.push(Element::from(iced::widget::Text::new(element.clone())));
         }
+        let elements = iced::widget::Column::with_children(elements).padding(10);
+
+
+        column
+            .push(elements)
+            .into()
     }
 }
 
 fn main() {
-    let app = RelmApp::new("relm4.example.simple");
-    app.run::<App>(0);
+    let _ = HelloWorld::run(iced::Settings::default());
 }
